3e06f70d8a75bdb2e28cc49f5c9f04c01f87c29b
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-20 18:22:06 -0600

    understanding the benefit of using orders in the example case of using duplicate names for variables.

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,11 @@ mod inventory {
     }
 }
 
+mod orders {
+    pub const MANAGER: &str = "Oliver Orderson";
+}
+
 fn main() {
     println!("The manager of our inventory is {}", inventory::MANAGER);
+    println!("The manager of our orders is {}", orders::MANAGER);
 }
